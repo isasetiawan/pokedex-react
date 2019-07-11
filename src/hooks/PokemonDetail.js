9059686b7cb8955c3a893b919b3d0d6183af68b3
isasetiawan <azaqokay@gmail.com>
2019-07-11 14:31:17 +0700

    PokemonDetail.js : add some detail to display

--- src/hooks/PokemonDetail.js
+++ src/hooks/PokemonDetail.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import { Modal, Image, Placeholder } from 'semantic-ui-react';
+import { Modal, Image, Placeholder, Header, StatisticGroup, Statistic } from 'semantic-ui-react';
 import { URL_FETCH } from '../const';
 
 const PokemonDetail = () => {
@@ -70,6 +70,55 @@ const PokemonDetail = () => {
                 <Modal.Header>{pokemon.name}</Modal.Header>
                 <Modal.Content image>
                     <Image wrapped size="medium" src={pokemon.image}></Image>
+                    <Modal.Description>
+                    <StatisticGroup size="tiny">
+                            <Statistic>
+                                <Statistic.Label>HP</Statistic.Label>
+                                <Statistic.Value>{pokemon.maxHP}</Statistic.Value>
+                            </Statistic>
+                            <Statistic>
+                                <Statistic.Label>CP</Statistic.Label>
+                                <Statistic.Value>{pokemon.maxCP}</Statistic.Value>
+                            </Statistic>
+                            <Statistic>
+                                <Statistic.Label>Flee Rate</Statistic.Label>
+                                <Statistic.Value>{pokemon.fleeRate}</Statistic.Value>
+                            </Statistic>
+                        </StatisticGroup>
+                        <Header>Height</Header>
+                        <StatisticGroup size="tiny">
+                            <Statistic>
+                                <Statistic.Label>Maximum</Statistic.Label>
+                                <Statistic.Value>{pokemon.height ? pokemon.height.maximum : ''}</Statistic.Value>
+                            </Statistic>
+                            <Statistic>
+                                <Statistic.Label>Minimum</Statistic.Label>
+                                <Statistic.Value>{pokemon.height ? pokemon.height.minimum : ''}</Statistic.Value>
+                            </Statistic>
+                        </StatisticGroup>
+
+                        <Header>Weight</Header>
+                        <StatisticGroup size="tiny">
+                            <Statistic>
+                                <Statistic.Label>Maximum</Statistic.Label>
+                                <Statistic.Value>{pokemon.weight ? pokemon.weight.maximum : ''}</Statistic.Value>
+                            </Statistic>
+                            <Statistic>
+                                <Statistic.Label>Minimum</Statistic.Label>
+                                <Statistic.Value>{pokemon.weight ? pokemon.weight.minimum : ''}</Statistic.Value>
+                            </Statistic>
+                        </StatisticGroup>
+                        <Header>Type</Header>
+                        <p>{pokemon.types ? pokemon.types.join(', ') :''}</p>
+                        <Header>Resistant</Header>
+                        <p>{pokemon.resistant ? pokemon.resistant.join(', ') :''}</p>
+                        <Header>Special Attacks</Header>
+                        <p>{pokemon.attacks ? pokemon.attacks.special.map(x=>x.name+', ') : ''}</p>
+                        <Header>Fast Attacks</Header>
+                        <p>{pokemon.attacks ? pokemon.attacks.fast.map(x=>x.name+', ') : ''}</p>
+                        <Header>Weaknesses</Header>
+                        <p>{pokemon.weaknesses ? pokemon.weaknesses.join(', ') :''}</p>
+                    </Modal.Description>
                 </Modal.Content>
                 </>
             :
