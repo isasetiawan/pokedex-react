c1623b27b65453a76c30c4801ae5f8fa97640414
isasetiawan <azaqokay@gmail.com>
2019-07-11 11:10:37 +0700

    PokemonCard.js : Created card for each pokemon in list

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Card, Grid, Image, Statistic, Divider, Header } from 'semantic-ui-react'
+
+const PokemonCard = ({image, name, classification, height, weight, maxCP, maxHP, types}) => {
+    return (
+            <Grid.Column>
+            <Card fluid>
+                <Image size="medium" centered height="100px" src={image}></Image>
+                <Card.Content>
+                    <Card.Header>
+                        <Header as="h1">{name}</Header>
+                    </Card.Header>
+                    <Card.Meta>
+                        <span className="date">HP/CP : {maxHP}/{maxCP}</span>
+                    </Card.Meta>
+
+                    <Divider></Divider>
+
+                    <Statistic.Group widths={1} size="mini">
+                        <Statistic>
+                            <Statistic.Label>Type</Statistic.Label>
+                            <Statistic.Value>{types.join(', ')}</Statistic.Value>
+                        </Statistic>
+                    </Statistic.Group>
+
+                    <Divider></Divider>
+
+                    <Statistic.Group widths={2} size="mini">
+                        <Statistic>
+                            <Statistic.Label>height</Statistic.Label>
+                            <Statistic.Value>{height.maximum}</Statistic.Value>
+                        </Statistic>
+                        <Statistic>
+                            <Statistic.Label>weight</Statistic.Label>
+                            <Statistic.Value>{weight.maximum}</Statistic.Value>
+                        </Statistic>
+                    </Statistic.Group>
+
+                </Card.Content>
+                <Card.Content extra>
+                    <b className="bold">Classification: </b>{classification}
+                </Card.Content>
+            </Card>
+            </Grid.Column>
+    )
+}
+
+export default PokemonCard
